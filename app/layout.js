3f48b2e57dd738f68fc92e7a77322dae272de81d
jrf <okayjrf@gmail.com>
2023-06-12 20:38:00 -0700

    fixes

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: "JR Fernandez's Website",
+  title: 'JR Fernandezs Website',
   description: 'Website for IT122',
 }
 
